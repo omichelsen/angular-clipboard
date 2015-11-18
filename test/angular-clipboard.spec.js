51e67a75b3228202510defd0e0ba67a446d65cc4
Cory Silva <coryasilva@users.noreply.github.com>
2015-11-18 06:54:52 -0800

    Update angular-clipboard.spec.js

--- test/angular-clipboard.spec.js
+++ test/angular-clipboard.spec.js
@@ -36,6 +36,7 @@ describe('angular-clipboard', function () {
     });
     
     it('should be caught by angular\'s digest cycle', function () {
+        spyOn(document, 'execCommand').and.returnValue(true);
         spyOn(scope, 'success').and.callThrough();
         elm.triggerHandler('click');
         expect(scope.copied).toEqual(true);
