a1b52305c02051448549cec9b373a2dc4c9d3666
Itai Lulu Koren <itailulu@Itais-MacBook-Pro.local>
2017-01-09 01:19:17 +0200

    WIP

--- test/angular-clipboard.spec.js
+++ test/angular-clipboard.spec.js
@@ -50,4 +50,76 @@ describe('angular-clipboard', function () {
     it('should feature detect and set supported', function () {
         expect(scope.supported).toEqual(true);
     });
+
 });
+
+describe('provider', function() {
+        var provider;
+        
+        /*beforeEach(inject(function (angularClipboard) {
+            provider = angularClipboard;
+        }));*/
+
+        beforeEach(function(){
+            var fakeModule = angular.module('test.app.config', function () {});
+            fakeModule.config( function (angularClipboardProvider) {
+                provider = angularClipboardProvider;
+            });
+            // Initialize test.app injector
+            module('angular-clipboard', 'test.app.config');
+
+            // Kickstart the injectors previously registered 
+            // with calls to angular.mock.module
+            inject(function () {});
+        });
+
+        beforeEach(angular.mock.inject(function ($rootScope, $compile) {
+            scope = $rootScope;
+            elm = $compile('<button clipboard text="textToCopy">Copy</button>')(scope);
+
+            scope.textToCopy = 'Copy me!';
+            scope.$digest();
+
+        }));
+
+        describe('with custom configuration', function () {
+            it('set default success/error callbacks', function () {
+                // check sanity
+                expect(provider).not.toBeUndefined();
+                // configure the provider
+                var successCallback = function() {scope.defaultSuccessCallback = true};
+                var errorCallback = function() {scope.defaultErrorCallback = true};
+                var obj = {
+                    success : successCallback,
+                    error: errorCallback
+                }
+                provider.configure({
+                    onCopiedDefaultCallback: obj.success,
+                    onErrorDefaultCallback: obj.error
+                });
+                expect(provider.$get().defaultOnCopied).toEqual(obj.success);
+                expect(provider.$get().defaultOnError).toEqual(obj.error);
+                
+                elm.triggerHandler('click');
+                expect(obj.success).toEqual(true);
+                expect(scope.defaultErrorCallback).toEqual(true);
+
+
+            });
+        });
+        
+        /*it('should exist', function() {
+            expect(provider).toBeDefined();
+        });
+
+        it('should be configurable', function() {
+            debugger;
+            provider.configure({
+                onCopiedDefaultCallback: function(){
+                    // don't do anything
+                }
+            })
+            //expect(provider.options).toBeDefined();
+        })*/
+
+    });
